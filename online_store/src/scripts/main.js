4c95aa106d5f58588b7722bf30946be9de11bf6b
i.shekhovcov <i.shekhovcov@cm.expert>
2021-05-07 01:09:42 +0300

    refactor styles for login modal, add registr modal layout

--- online_store/src/scripts/main.js
+++ online_store/src/scripts/main.js
@@ -1,15 +1,28 @@
 const btnLogin = document.querySelector('.btn-login');
+const btnOpenModalReg = document.querySelector('#open-modal-reg');
+const modals = document.querySelectorAll('.modal');
 const modalLogin = document.querySelector('#modal-login');
+const modalRegistr = document.querySelector('#modal-registr');
 
 btnLogin.addEventListener('click', () => {
     modalLogin.classList.remove('hide');
     modalLogin.classList.add('show');
 });
 
-modalLogin.addEventListener('click', (e) => {
-    const targetModal = e.target.closest(".modal__content");
-    if(!targetModal) {
-        modalLogin.classList.add('hide');
-        modalLogin.classList.remove('show');
-    }
+modals.forEach(modal => {
+    modal.addEventListener('click', (e) => {
+        const targetModal = e.target.closest(".modal__content");
+        if(!targetModal) {
+            modal.classList.add('hide');
+            modal.classList.remove('show');
+        }
+    });
+})
+
+btnOpenModalReg.addEventListener('click', (e) => {
+    modalLogin.classList.add('hide');
+    modalLogin.classList.remove('show');
+
+    modalRegistr.classList.remove('hide');
+    modalRegistr.classList.add('show');
 });
