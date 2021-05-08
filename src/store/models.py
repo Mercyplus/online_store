c71fd26500c78cf1daec7455f24fce71adb55848
Mercyplus <sobachka1966@mail.ru>
2021-05-08 20:36:17 +0300

    add be

---
+++ src/store/models.py
@@ -0,0 +1,44 @@
+from django.db import models
+from django.urls import reverse
+
+
+class Category(models.Model):
+	name = models.CharField(max_length=250, unique=True)
+	slug = models.SlugField(max_length=250, unique=True)
+	description = models.TextField(blank=True)
+	image = models.ImageField(upload_to='category', blank=True)
+
+	class Meta:
+		ordering = ('name',)
+		verbose_name = 'Категория'
+		verbose_name_plural = 'Категории'
+
+	def get_url(self):
+		return reverse('products_by_category', args=[self.slug])
+
+	def __str__(self):
+		return self.name
+
+
+class Product(models.Model):
+	name = models.CharField(max_length=250, unique=True, verbose_name='Имя продукта')
+	slug = models.SlugField(max_length=250, unique=True, verbose_name='Слаг продукта')
+	description = models.TextField(blank=True, verbose_name='Описание продукта')
+	category = models.ForeignKey(Category, verbose_name='Категория', on_delete=models.CASCADE)
+	price = models.DecimalField(max_digits=10, decimal_places=0, verbose_name='Цена')
+	image = models.ImageField(upload_to='products', blank=True, verbose_name='Изображение')
+	stock = models.IntegerField(null=True, blank=True, verbose_name='Колличество товара на складе')
+	available = models.BooleanField(default=True, verbose_name='В наличии')
+	created = models.DateTimeField(auto_now_add=True)
+	updated = models.DateTimeField(auto_now=True)
+
+	class Meta:
+		ordering = ('name',)
+		verbose_name = 'Товар'
+		verbose_name_plural = 'Товары'
+
+	def get_url(self):
+		return reverse('product_detail', args=[self.category.slug, self.slug])
+
+	def __str__(self):
+		return self.name
---
+++ src/store/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('category/<slug:category_slug>', views.home, name='products_by_category'),
+    path('category/<slug:category_slug>/<slug:product_slug>', views.product, name='product_detail'),
+    path('account/create/', views.signUpView, name='signup'),
+    path('account/login/', views.loginView, name='login'),
+    path('account/signout/', views.signoutView, name='signout'),
+]
