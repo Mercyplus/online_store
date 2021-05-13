5a9f8cf3a3ac542ddc0fe0ae69b2ac4e4428c6bf
Mercyplus <sobachka1966@mail.ru>
2021-05-14 01:39:18 +0300

    add widgets for count

--- src/cart/forms.py
+++ src/cart/forms.py
@@ -1,13 +1,14 @@
 from django import forms
 
 
-PRODUCT_QUANTITY_CHOICES = [(i, str(i)) for i in range(1, 21)]
+PRODUCT_QUANTITY_CHOICES = [(i, str(i)) for i in range(1, 16)]
 
 
 class CartAddProductForm(forms.Form):
     quantity = forms.TypedChoiceField(
                                 choices=PRODUCT_QUANTITY_CHOICES,
-                                coerce=int)
+                                coerce=int, label='Количество',
+                                widget=forms.Select(attrs={"class": "product__count"}))
     update = forms.BooleanField(required=False,
                                 initial=False,
                                 widget=forms.HiddenInput)
